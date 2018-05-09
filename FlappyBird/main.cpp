3decee17c670bd4728f1e0f487e2bbc19eaf9e5c
Nguyen Binh Kien <csvietteam@gmail.com>
2018-05-09 23:57:45 +0700

    * Add comment fix linker error.

--- FlappyBird/main.cpp
+++ FlappyBird/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <SDL.h>
-#undef main
+#undef main //* Fix LNK: unresolved external symbol SDL_main referenced in function main_utf8
 
 int main(int argc, char **argv) {
 	return 0;
