a5489218e7fca4d630686e21ff0ab217b532dc72
Nguyen Binh Kien <csvietteam@gmail.com>
2018-05-11 16:43:58 +0700

    + Add prototype state.

---
+++ FlappyBird/StateBase.cpp
@@ -0,0 +1,23 @@
+#include "StateBase.h"
+
+
+
+StateBase::StateBase()
+{
+}
+
+
+StateBase::~StateBase()
+{
+}
+
+bool StateBase::Update() {
+	return true;
+}
+void StateBase::Render() {
+	
+}
+
+void StateBase::Destroy() {
+
+}
