b945d57cc2bab8eca7de3cf1d1db23af2dbc92f4
Nguyen Binh Kien <csvietteam@gmail.com>
2018-06-02 10:48:27 +0700

    + Add base, Play animation with speed

--- FlappyBird/BackGround.cpp
+++ FlappyBird/BackGround.cpp
@@ -13,7 +13,7 @@ BackGround::~BackGround()
 bool BackGround::Init() {
 	Position s_start(0, 0);
 	for (int i = 0; i < COUNT_BACKGROUND; i++) {
-		mBackGround[i].Init(Position(0, 0), SIZE_BACKGROUND_X, SIZE_BACKGROUND_Y, s_start, SIZE_BACKGROUND_X, SIZE_BACKGROUND_Y);
+		mBackGround[i].Init(Position(0, 0), SIZE_BACKGROUND_X, SIZE_BACKGROUND_Y, s_start, SIZE_BACKGROUND_X, SIZE_BACKGROUND_Y - 50);
 		mBackGround[i].loadTextTure(i < (COUNT_BACKGROUND / 2) ? "asset/sprites/background-day.png" : "asset/sprites/background-night.png");
 		TextureManager::getInstance()->AddTexture(&mBackGround[i]);
 		s_start = Position(s_start.x + SIZE_BACKGROUND_X, s_start.y);
@@ -31,7 +31,14 @@ bool BackGround::Init() {
 	mc.Init(Position(0, 0), SIZE_MC_X, SIZE_MC_Y, s_start, SIZE_MC_X, SIZE_MC_Y, 3);
 	mc.loadTextTure("asset/sprites/sprite.png");
 	TextureManager::getInstance()->AddTexture(&mc);
-
+	
+	s_start = Position(0, 350);
+	for (int i = 0; i < COUNT_GROUND; i++) {
+		mGround[i].Init(Position(0, 0), SIZE_GROUND_X, SIZE_GROUND_Y, s_start, SIZE_GROUND_X, SIZE_GROUND_Y);
+		mGround[i].loadTextTure("asset/sprites/base.png");
+		TextureManager::getInstance()->AddTexture(&mGround[i]);
+		s_start = Position(s_start.x + SIZE_GROUND_X, 350);
+	}
 	return true;
 }
 bool BackGround::Pause() {
@@ -58,10 +65,9 @@ bool BackGround::Update() {
 			tmp_DPosition.x = tmp_last.x + SIZE_PIPE_X + rand() % 10 + 50;
 			tmp_DPosition.y = 150 + rand() % 50;
 		}
-		SDL_Log(" Pipe i :: %d  - position :: (%d-%d)", i, tmp_DPosition.x, tmp_DPosition.y);
 		mPipe[i].setDPosition(tmp_DPosition);
 	}
-	mc.playAnimation();
+	mc.playAnimation(50);
 	return true;
 }
 
--- FlappyBird/BackGround.h
+++ FlappyBird/BackGround.h
@@ -3,11 +3,14 @@
 
 #define COUNT_BACKGROUND 12
 #define COUNT_PIPE 8
+#define COUNT_GROUND 4
+
 class BackGround
 {
 private:
 	Texture mBackGround[COUNT_BACKGROUND];
 	Texture mPipe[COUNT_PIPE];
+	Texture mGround[COUNT_GROUND];
 	Texture mc;
 	int speed = 1;
 	const int SIZE_MC_X = 34;
@@ -16,6 +19,8 @@ private:
 	const int SIZE_PIPE_Y = 320;
 	const int SIZE_BACKGROUND_X = 288;
 	const int SIZE_BACKGROUND_Y = 512;
+	const int SIZE_GROUND_X = 366;
+	const int SIZE_GROUND_Y = 112;
 public:
 	BackGround();
 	~BackGround();
